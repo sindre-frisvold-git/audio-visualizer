f38cc662f382966613fa92a8c554ca686abc3fef
Sindre <sinfr91@gmail.com>
2022-03-07 22:37:16 +1300

    add slider for thickness

--- lightning.js
+++ lightning.js
@@ -62,7 +62,7 @@ var ctx = c.getContext("2d");
 let segmentSpread = 5             // lower values causes more spread between each segment
 let lightningExtension = 20       // lightning length, higher values shortens lightning
 let roughness = 2;                // lower values makes lightning more rough
-let lightningThickness = 0.1      // adjusts widtth of lightning bolt
+let lightningThickness = 1      // adjusts widtth of lightning bolt
 let testVar = 1                   // placeholder, ignore
 let finalSpread = 0               // lightning spread at the bottom ov canvas
 let color = "hsl(180, 80%, 80%)"; // color of lightning
@@ -73,10 +73,10 @@ let color = "hsl(180, 80%, 80%)"; // color of lightning
 //   // take an array of values and return the average
 
 
-const normaliser = (max, min, value, floorValue = 0) =>{
+const normaliser = (max, min, value, offset = 0) =>{
   // create function that normalises a range to a value between 1 and 0
   // ?add 3rd parameter for value to be parsed and return final value.
-  let ratio =((value - floorValue)/(256 - floorValue))
+  let ratio =((value - offset)/(256 - offset))
   let output = (max - min) * ratio
   if((output + min)<0) return min
   return output + min
@@ -89,7 +89,7 @@ const experimentalExponent = (max, min, value) =>{
   let ratio = value/256
   if(range === (-1))return min -(10**ratio)/10
   if(range === 1)return (10**ratio)/10 + min
-  if(range > (-1))return min -(-range)**ratio
+  if(range < -1)return min -(-range)**ratio
   return range**ratio+min
 }
 // link output to lightning variables
@@ -107,20 +107,31 @@ const inputReducer = (arr) =>{
   }
   return result
 }
+// dom constants
+
+const thicknessRange = document.getElementById('thickness')
+const thicknessFrequency = document.getElementById('thicknessFrequency')
+let thickness = thicknessRange.valueAsNumber
+// thicknessRange.addEventListener('oninput', (e) =>{
+//   console.log(e)
+//   thickness = e.currentTarget.value
+// })
+
 // create function to change lightning variables
 // add array for staoring previosu values for averaging puroposes
 let rainbow = 0
 const lightningModifier = () => {
+  requestAnimationFrame(lightningModifier)
 // link one variable to audio output
 analyser.getByteFrequencyData(data);
 let frequencyData = inputReducer(data)
-segmentSpread = normaliser(1, 15, frequencyData[4]);
-lightningThickness = exponentialNormaliser(30, 1, frequencyData[10]);
-finalSpread = exponentialNormaliser(50, 1, frequencyData[0])/100
+segmentSpread = experimentalExponent(1, 30, frequencyData[3]);
+lightningThickness = exponentialNormaliser(thicknessRange.valueAsNumber, 1, frequencyData[thicknessFrequency.value]);
+finalSpread = 0
 roughness = experimentalExponent(1.5, 2.5, frequencyData[6])
-color = `hsl(${exponentialNormaliser(360, 0, frequencyData[6])}, 80%, 80%)`
+color = `hsl(${experimentalExponent(360, 180, frequencyData[1])}, 80%, 80%)`
 // lightningExtension = Math.random() * normaliser(height/2, 0, frequencyData[5])
-requestAnimationFrame(lightningModifier)
+
 }
 
 
